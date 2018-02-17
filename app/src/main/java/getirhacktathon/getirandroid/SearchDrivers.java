f8a4a50bd8ffd0107f8add06937fa40645ef2f6c
atakanguney <atakanguney58@hotmail.com>
2018-02-17 13:05:55 +0300

    create GoogleMapsActivity

--- app/src/main/java/getirhacktathon/getirandroid/SearchDrivers.java
+++ app/src/main/java/getirhacktathon/getirandroid/SearchDrivers.java
@@ -1,5 +1,6 @@
 package getirhacktathon.getirandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SearchDrivers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_drivers);
+
+        Intent showMap = new Intent(this, GoogleMapsActivity.class);
+        startActivity(showMap);
     }
 }
